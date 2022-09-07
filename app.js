edb918255541df77f7fa378f43b9646269adeb62
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2022-09-07 14:28:33 +0530

    mistake

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-const BillAmount = document.querySelector("#billAmount");
-const dateOfBirth = document.querySelector(".date-of-birth");
-const btnCheck = document.querySelector("#check-number");
-const LuckyNumber = document.querySelector(".lucky-number");
-const Message = document.querySelector(".Message");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check-btn");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+checkBtn.addEventListener("click", function calidedateBillAndCashAmount() {
+  hidemessage();
 
-function compareValues(sum, luckyNumber) {
-  if (sum % luckyNumber === 0) {
-    Message.innerText = "your birthday date is lucky";
+  if (billAmount.value > 0) {
+    //12
+    if (cashGiven.value > billAmount.value) {
+      //2022>12 // true
+      const amountTobeReturned = cashGiven.value - billAmount.value; //2022-12=2010
+      calculateChange(amountTobeReturned);
+    } else {
+      showMessage("Do you wanna wash plates?");
+    }
   } else {
-    Message.innerText = "your birthday date is not lucky";
+    showMessage("Invalid Bill Amount");
+  }
+});
+function calculateChange(amountTobeReturned) {
+  //2010
+  //go over all the avilable
+  for (let i = 0; i < availableNotes.length; i++) {
+    // no of notes need for the denomination
+    const numberOfNotes = Math.trunc(amountTobeReturned / availableNotes[i]);
+    //2010 /2000 = 1 || 10 - 500 = 0
+    //amount left after calculating in the number of notes needed
+    amountTobeReturned %= availableNotes[i]; //2010 % 2000 = 10 || 10% 500 =10
+    // updateing  the no of notes in the table for the current amount
+    noOfNotes[i].innerText = numberOfNotes;
   }
 }
-function checkBirthdateIsLucky() {
-  const dob = dateOfBirth.value;
-  const sum = calculateSum(dob);
-  if (sum && dob) {
-    compareValues(sum, LuckyNumber.value);
-  }
-  Message.innerText = "please enter the both the field";
+function hidemessage() {
+  errorMessage.style.display = "none";
 }
-function calculateSum(dob) {
-  dob = dob.replaceAll("-", "");
-  sum = 0;
-  for (let index = 0; index < dob.length; index++) {
-    sum = sum + Number(dob.charAt(index));
-  }
-  return sum;
+function showMessage(msg) {
+  errorMessage.style.display = "block";
+  errorMessage.innerText = msg;
 }
-
-btnCheck.addEventListener("click", checkBirthdateIsLucky);
